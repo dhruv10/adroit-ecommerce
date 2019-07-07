244dc3cc92b8aeca62a212283fdc931b6dfa8d87
Dhruv Bhatnagar <dhruvbhatnagar10@gmail.com>
2019-07-07 21:39:39 +0530

    Importing firebase and settign the firebase config

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import * as firebase from "firebase/app";
+
+// Add the Firebase products that you want to use
+import "firebase/auth";
 
 Vue.config.productionTip = false
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCOsHglPChm8IfjiGZst0NrxAa29eGYgFg",
+  authDomain: "adroit-ecommerce.firebaseapp.com",
+  databaseURL: "https://adroit-ecommerce.firebaseio.com",
+  projectId: "adroit-ecommerce",
+  storageBucket: "",
+  messagingSenderId: "600000770038",
+  appId: "1:600000770038:web:69f68ea53734cfb1"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
